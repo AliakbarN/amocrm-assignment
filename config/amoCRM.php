696b659e8b7e123c904c4133cc638ef7bd6dc6ad
Aliakbar <anurillakhonov@team.amocrm.com>
2024-01-06 11:00:51 +0500

    edited amoCrm config

--- config/amoCRM.php
+++ config/amoCRM.php
@@ -3,6 +3,6 @@
 return [
     'client_id'           => 'e095090d-d4d5-4202-aabc-cd0f3f40532a',
     'client_secret'       => 'DUx6xIhMA6FhIRJlLPXzJkKvLuwC6Chxb9R3ffYAzKfRpfgY41pL0NZAZRq1NOtV',
-    'client_redirect_url' => 'https://7c37-94-158-52-23.ngrok-free.app/api/get-credentials',
+    'client_redirect_url' => 'https://d58d-94-158-52-23.ngrok-free.app/api/get-credentials',
     'base_domain'         => 'anurillakhonov.amocrm.ru'
 ];
